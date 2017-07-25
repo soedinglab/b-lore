Fixed exponential overflow in logmL

blore/inference/quasi_laplace.py
@@ -28,9 +28,9 @@ def czcompgrad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, get_gradient=T
     '''
 
     _path = os.path.dirname(__file__)
-    lib = np.ctypeslib.load_library('../lib/margloglik.so', _path)
+    lib = np.ctypeslib.load_library('../lib/margloglik_dev.so', _path)
     ccomps = lib.margloglik_zcomps
-    ccomps.restype = ctypes.c_int
+    ccomps.restype = ctypes.c_double
     ccomps.argtypes = [ctypes.c_int,                                                                   # nvar, number of SNPs
                        ctypes.c_int,                                                                   # nhyp, number of hyperparameters
                        ctypes.c_int,                                                                   # nfeat, number of features
@@ -59,7 +59,7 @@ def czcompgrad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, get_gradient=T
     zcomps = np.zeros(zlen)
     grad = np.zeros(nhyp)
 
-    success = ccomps(v.shape[0],
+    logmL = ccomps(v.shape[0],
                      nhyp,
                      feat.shape[0],
                      zlen,
@@ -79,7 +79,7 @@ def czcompgrad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, get_gradient=T
                      get_gradient,
                      is_covariate)
 
-    return zcomps, grad
+    return logmL, zcomps, grad
 
 
 def margloglik_zcomps(pi, mu, sig2, z, v, mureg, reg2, prec, is_covariate=False):
@@ -105,9 +105,9 @@ def margloglik_zcomps(pi, mu, sig2, z, v, mureg, reg2, prec, is_covariate=False)
     nhyp = 0
     feat = np.array([])
 
-    zcomps, grad = czcompgrad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, get_gradient, is_covariate)
+    logmL, zprob, grad = czcompgrad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, get_gradient, is_covariate)
 
-    return zcomps
+    return zprob
 
 
 def mll_grad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, is_covariate=False):
@@ -118,153 +118,5 @@ def mll_grad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, is_covariate=Fal
     '''
     get_gradient = True
 
-    zcomps, grad = czcompgrad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, get_gradient, is_covariate)
-    pl = np.sum(zcomps)
-    # To avoid numerical overflow error, negligible marginal likelihoods are rounded off to low values
-    if pl < 1e-200:
-        print("Warning: Overflow in calculating logarithm of marginal likelihood. Applying constraints")
-        pl = 1e-200
-    mll = np.log(pl)
-    return mll, grad
-
-
-# ====================================================================================================================================
-# Old python implementations. Kept for legacy reasons
-#
-#def grad_locus_comps(pi, mu, sig2, nhyp, feat, z, zcomps, intmu, intlam_inv, is_covariate=False):
-#    ''' Computes the gradient of the log marginal likelihood for each locus.
-#        Inputs:
-#              pi: proportion of causal distribution, array of N floats, only applicable for SNP loci
-#              mu: mean of the causal distribution, array of N floats, 0 for covariate locus
-#              sig2: variance of the causal distribution, array of N floats, different for SNP loci and covariate locus
-#              feat: array of features, shape K x I for SNP loci, Kc x J for covariate loci
-#              nhyp: number of hyperparameters
-#              k: number of SNP features, K
-#              kc: number of covariate features, Kc
-#              z: all the zstates for the locus // could be empty for the 0 z-state // all 1's for covariate locus
-#              zcomps: pre-calculated p(z)F(z) for each z-state of the locus
-#              intmu: means of the multivariate Gaussian obtained from integration // list of arrays for all zstates
-#              intlam_inv: Inverse of precision matrix of the multivariate Gaussian obtained from integration // list of arrays for all zstates
-#        Returns:
-#              der: The derivates with respect to all hyperparameters, array of (3 x K + Kc) floats
-#    '''
-#
-#    zlen  = len(z)         # Number of zstates for the locus
-#    znorm  = [len(z[i]) for i in range(zlen)] # norm of all z-states
-#    zmask  = [np.array(z[i]) for i in range(zlen)] # convert to array for masking
-#
-#    der = np.zeros(nhyp)
-#
-#    zcomps = np.array(zcomps)
-#    pz = zcomps / np.sum(zcomps)
-#
-#    if not is_covariate: # Calculate values for SNP loci / not for hypothetical covariate locus
-#
-#        k = feat.shape[0]
-#
-#        # Partial derivative w.r.t. PI
-#        pi_grad = np.zeros((zlen, k))
-#        for i in range(zlen):
-#            mask = np.zeros(pi.shape)
-#            if not znorm[i] == 0:
-#                mask[zmask[i]] = 1
-#            pi_grad[i, :] = np.einsum('i, ki -> k', mask - pi, feat)
-#        der[0:k] = np.einsum('i, ik -> k', pz, pi_grad)
-#    
-#        # Partial derivative w.r.t. MU
-#        mu_grad = np.array([np.einsum('i, ki -> k', \
-#                                      (intmu[i] - mu[zmask[i]]) / sig2[zmask[i]], \
-#                                      feat[:, zmask[i]]) \
-#                           if not znorm[i]==0 else np.zeros(k) for i in range(zlen)])
-#        der[k:2*k] = np.einsum('i, ik -> k', pz, mu_grad)
-#    
-#        # Partial derivative w.r.t. SIGMA
-#        a = [(np.square(mu[zmask[i]] - intmu[i]) - sig2[zmask[i]] + np.diag(intlam_inv[i])) / sig2[zmask[i]] \
-#                           if not znorm[i]==0 else np.array([]) for i in range(zlen)]    
-#        sig_grad = np.array([np.einsum('i, ki -> k', a[i], feat[:, zmask[i]]) \
-#                           if not znorm[i]==0 else np.zeros(k) for i in range(zlen)])
-#        der[2*k:3*k] = 0.5 * np.einsum('i, ik -> k', pz, sig_grad)
-#
-#    else:
-#
-#        kc = feat.shape[0]
-#        # Partial derivative w.r.t. SIGMA_W
-#        a = [(np.square(mu[zmask[i]] - intmu[i]) - sig2[zmask[i]] + np.diag(intlam_inv[i])) / sig2[zmask[i]] \
-#                           if not znorm[i]==0 else np.array([]) for i in range(zlen)]
-#        sig_grad = np.array([np.einsum('i, ki -> k', a[i], feat[:, zmask[i]]) \
-#                           if not znorm[i]==0 else np.zeros(kc) for i in range(zlen)])
-#        der[-kc:] = 0.5 * np.einsum('i, ik -> k', pz, sig_grad)
-#
-#    return der
-#
-#
-#def margloglik_zcomps_py(pi, mu, sig2, z, v, reg2, prec, is_covariate=False):
-#    ''' Calculates the log marginal likelihood for all z-states in a given locus
-#        Inputs:
-#              pi: proportion of causal distribution, array of N floats, only applicable for SNP loci
-#              mu: mean of the causal distribution, array of N floats, 0 for covariate locus
-#              sig2: variance of the causal distribution, array of N floats, different for SNP loci and covariate locus
-#              z: all the zstates for the locus // could be empty for the 0 z-state // all 1's for covariate locus
-#              v: effect size / coefficients after logistic regression, array of N floats
-#              reg2: the variance of the regularizer, same for SNP loci and covariate locus, float value
-#              prec: the precision matrix, obtained from the logistic regression, N x N array
-#              is_covariate: boolean to mark the covariate locus
-#         Returns:
-#              muz: means of the multivariate Gaussian obtained from integration
-#              lamz_inv : Inverse of precision matrix of the multivariate Gaussian obtained from integration
-#              zcomps: 
-#    '''
-#
-#    zlen   = len(z) # number of z-states
-#    znorm  = [len(z[i]) for i in range(zlen)] # norm of all z-states
-#    zmask  = [np.array(z[i]) for i in range(zlen)] # convert to array for masking
-#
-#    # Log of probability of z given Xi and Theta => ln p(z | Xi, Theta)
-#    # For each locus, this is calculated from the array of pi
-#    # For covariate locus, p(z | Xi, Theta) is 1, hence ln p is zero.
-#    '''Be careful: For any SNP, if pi is zero, then log(pi) becomes undefined
-#                   For any SNP, if pi is 1, then log(1-pi)  becomes undefined
-#    '''
-#
-#    logpz = [0.0 for i in range(zlen)] # ln(1) = 0
-#    if not is_covariate: # Calculate values for SNP loci / not for hypothetical covariate locus
-#        for i in range(zlen):
-#            mask = np.zeros(pi.shape, dtype=bool)
-#            if not znorm[i] == 0:
-#                mask[zmask[i]] = True
-#            logpz[i] = np.sum(np.log(pi[mask])) + np.sum(np.log(1.0 - pi[~mask]))
-#
-#    # Log of second component => ln F(z | Xi, Theta)
-#    # First we calculate 2 pre-requisite terms: Lambda_z and V_z
-#
-#    # Lambda_z
-#    # Precision matrix of the multivariate Gaussian obtained from integration.
-#    # For locus with ||z|| = 0, it is an empty array
-#    # For covariate locus, input sig2 is different, everything else is same
-#    def __getlamz(mask):
-#        ''' prec, sig2, reg2 comes from global scope '''
-#        x = np.array(prec[mask][:,mask])
-#        x[np.diag_indices(len(mask), ndim=2)] += 1 / sig2[mask] - 1 / reg2
-#        return x
-#
-#    lamz     = [__getlamz(zmask[i])    if not znorm[i] == 0 else np.array([]) for i in range(zlen)]
-#    lamz_inv = [np.linalg.inv(lamz[i]) if not znorm[i] == 0 else np.array([]) for i in range(zlen)]
-#
-#    # V_z 
-#    # Vector of means of the multivariate Gaussian obtained from integration
-#    # For locus with ||z|| = 0, it is an empty array
-#    # For covariate locus, input mu, sig2 are different, everything else is same
-#    vzinner = np.dot(prec, v) + (mu / sig2) # + (0 / reg2) 
-#    muz = [np.dot(lamz_inv[i], vzinner[zmask[i]]) if not znorm[i] == 0 else np.array([]) for i in range(zlen)]
-#
-#    # Finally, we calculate the 4 terms of ln F
-#    t1  = [np.sum(np.log(1 / sig2[zmask[i]]))                    if not znorm[i] == 0 else 1 for i in range(zlen)]
-#    t2  = [np.linalg.slogdet(lamz[i])                       if not znorm[i] == 0 else (1, -1) for i in range(zlen)]
-#    t3  = [np.sum(np.square(mu[zmask[i]]) / sig2[zmask[i]]) if not znorm[i] == 0 else 0 for i in range(zlen)]
-#    t4  = [np.einsum('i, ij, j', muz[i], lamz[i], muz[i])   if not znorm[i] == 0 else 0 for i in range(zlen)]
-#
-#    logfz = [(t2[i][0], 0.5 * (t1[i] - t2[i][1] - t3[i] + t4[i])) for i in range(zlen)]
-#
-#    zcomps = [np.exp(logpz[i] + logfz[i][1]) for i in range(zlen)]
-# 
-#    return muz, lamz_inv, zcomps
+    logmL, zcomps, grad = czcompgrad(pi, mu, sig2, z, v, nhyp, feat, mureg, reg2, prec, get_gradient, is_covariate)
+    return logmL, grad